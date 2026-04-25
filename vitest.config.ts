83c492ca6be9cce41acad19859b87003ce9133f9
Yevhenii Orhanistyi <organistiy@gmail.com>
2026-04-25 14:19:07 +0200

    chore: install and configure vitest

---
+++ vitest.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'node',
+    include: ['src/**/*.unit.spec.ts'],
+    coverage: {
+      provider: 'v8',
+      include: ['src/**/*.ts'],
+      exclude: [
+        'src/main.ts',
+        'src/**/*.module.ts',
+        'src/**/*.dto.ts',
+        'src/**/*.entity.ts',
+        'src/**/*.types.ts',
+        'src/prisma/**',
+      ],
+      thresholds: {
+        lines: 90,
+        branches: 85,
+      },
+      reporter: ['text', 'lcov'],
+    },
+  },
+});
